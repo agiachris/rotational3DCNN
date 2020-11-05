fc6cc70c15f3c59d9702ece56e42405f890a05bc
agiachris <c.agia@yahoo.com>
2020-11-04 20:12:22 -0500

    implement support methods in trainer, implement metric tracker
    Signed-off-by: agiachris <c.agia@yahoo.com>

--- metric.py
+++ metric.py
@@ -1,33 +1,56 @@
 import numpy as np
 import torch
-import torch.utils.data
+
+'''
+example usage 
+data = [[[[0, 0],[0, 0]],[[0, 1],[1, 1]]], [[[1, 1],[0, 1]],[[0, 0],[1, 1]]]]
+data_tensor = torch.FloatTensor(data)
+label = [[[[1, 0],[0, 1]],[[0, 1],[1, 1]]], [[[0, 1],[1, 0]],[[1, 1],[1, 1]]]]
+label_tensor = torch.FloatTensor(label)
+metric = Metric()
+print(metric.get_iou_per_batch(data_tensor, label_tensor))
+print(metric.get_accuracy_per_batch(data_tensor, label_tensor))
+'''
+
 
 class Metric:
-  # takes Height x Width x Length tensor filled with 0/1
-  def get_accuracy(pred, labels):
-      # gets voxel-wise accuracy per batch
-      correctness = torch.eq(pred, labels)
-      accuracy = torch.sum(correctness).item()/pred.nelement()
-      return accuracy
+    # takes Height x Width x Length tensor filled with 0/1
+    def get_accuracy_per_object(self, pred, labels):
+        # gets voxel-wise accuracy a signle object
+        correctness = torch.eq(pred, labels)
+        accuracy = torch.sum(correctness).item() / pred.nelement()
+        return accuracy
 
-  # takes Batch x Height x Width x Length tensor filled with 0/1
-  def get_mean_accuracy(pred, labels):
-    # gets accuracy for all data
-    accuracy = [get_accuracy(pred[i], labels[i]) for i in range(len(pred))]
-    return np.array(accuracy).mean()
+    # takes Batch x Height x Width x Length tensor/array filled with 0/1
+    def get_accuracy_per_batch(self, pred, labels):
+        # gets accuracy for a batch
+        accuracy = [self.get_accuracy_per_object(pred[i], labels[i]) for i in range(len(pred))]
+        return np.array(accuracy).mean()
 
-  # takes Height x Width x Length tensor filled with 0/1
-  def get_iou(pred, labels):
-      # gets intersection over union for a signle batch
-      pred = pred.squeeze(1)
-      labels = labels.squeeze(1)
-      intersection = np.logical_and(labels, pred)
-      union = np.logical_or(labels, pred)
-      iou = torch.sum(intersection) / torch.sum(union)
-      return iou
-  
-  # takes Batch x Height x Width x Length tensor filled with 0/1
-  def get_mean_iou(pred, labels):
-    # gets intersection over union for all data
-    iou = [get_iou(pred[i], labels[i]) for i in range(len(pred))]
-    return np.array(iou).mean()
+    # takes Number_of_batches x Batch x Height x Width x Length tensor/array filled with 0/1
+    def get_accuracy_for_all_data(self, pred, labels):
+        # gets intersection over union for all data
+        iou = [self.get_accuracy_per_batch(pred[i], labels[i]) for i in range(len(pred))]
+        return np.array(iou).mean()
+
+    # takes Height x Width x Length tensor filled with 0/1
+    def get_iou_per_object(self, pred, labels):
+        # gets intersection over union for a signle object
+        pred = pred.squeeze(1)
+        labels = labels.squeeze(1)
+        intersection = np.logical_and(labels, pred)
+        union = np.logical_or(labels, pred)
+        iou = torch.sum(intersection) / torch.sum(union)
+        return iou
+
+    # takes Batch x Height x Width x Length tensor/array filled with 0/1
+    def get_iou_per_batch(self, pred, labels):
+        # gets intersection over union for a batch
+        iou = [self.get_iou_per_object(pred[i], labels[i]) for i in range(len(pred))]
+        return np.array(iou).mean()
+
+    # takes Number_of_batches x Batch x Height x Width x Length tensor/array filled with 0/1
+    def get_iou_for_all_data(self, pred, labels):
+        # gets intersection over union for all data
+        iou = [self.get_iou_per_batch(pred[i], labels[i]) for i in range(len(pred))]
+        return np.array(iou).mean()
--- train.py
+++ train.py
@@ -1,65 +1,121 @@
 import os
+import shutil
 import argparse
 import torch
 import torch.nn as nn
 import torch.functional as F
 from torch.utils.data import DataLoader
-from data.shapenet import ShapeNet
+from dataset.shapenet import ShapeNet
+from metric import Metric
 from utils import *
 
 
 class Trainer:
 
     def __init__(self, args):
+        """
+        Create Trainer object to which handles the training and evaluation of a specified model,
+        the tracking of computed metrics, and saving of results / checkpoints.
 
-        # directories
-        base_dir = os.path.realpath(args.config)
+        :param args: ArgParse object which holds the path the experiment configuration file along
+                     with other key experiment options.
+        """
+        osj = os.path.join
 
-        # acquire configuration file
-        config = get_config(args.config)
+        # get experiment configuration file
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        config = get_config(base_path, args.config)
         self.config = config
 
-        # setup saving operations
+        # create experiment, logging, and model checkpoint directories
+        self.exp_dir = osj(osj(base_path, config['exp_dir']), config['exp_name'])
+        self.log_path = osj(self.exp_dir, "logs")
+        self.model_path = osj(self.exp_dir, "checkpoints")
+        if os.path.exists(self.exp_dir):
+            print("Error: This experiment already exists")
+            print("Cancel Session:    0")
+            print("Overwrite:         1")
+            if input() == 0:
+                shutil.rmtree(self.exp_dir)
+            else:
+                exit(1)
+        os.mkdir(self.exp_dir)
+        os.mkdir(self.log_path)
+        os.mkdir(self.model_path)
 
-        # load model (pre-trained or saved) - have a model configuration
-        self.model = get_model(config)
+        # Model - build in pre-trained load
+        model = get_model(config)
+        self.model = model
 
-        # load datasets - create dataloaders (use config file)
-        self.dataset = ShapeNet(config)
-
-
-        # setup metric global metric structures (loss, metric classes)
+        # Loss, Optimizer
         self.criterion = nn.BCEWithLogitsLoss()
+        self.optimizer = torch.optim.Adam(model.parameters(),
+                                          lr=config['lr'],
+                                          weight_decay=config['dr'])
 
+        # Dataset and DataLoader
+        self.dataset = ShapeNet(config)
+        self.loader = DataLoader(self.dataset, batch_size=config['batch_size'],
+                                 shuffle=True, num_workers=1, drop_last=False)
 
-    def train(self):
+        # Metrics
+        self.metrics = Metric()
+        self.train_metrics = MetricTracker(config, self.exp_dir, "train")
+        self.val_metrics = MetricTracker(config, self.exp_dir, "val")
         self.epoch = 0
 
-        # loop over epochs
+    def train(self):
+        """Train the model over configured epochs and track
+        metrics through training. Save the metrics once the training
+        commences.
+        """
         for self.epoch in self.config['num_epochs']:
+            # train and evaluate from an epoch
             self.run_epoch()
-            self.save()
 
-        self.save_metrics()
-        return 0
+            # average metrics over epoch and store
+            self.train_metrics.store_epoch()
+            self.val_metrics.store_epoch()
+
+            # save model checkpoint
+            self.save_model()
+
+        # save scores across epochs
+        self.train_metrics.save()
+        self.val_metrics.save()
 
     def run_epoch(self):
-
+        """Training and evaluation loop for a given epoch.
+        """
         # run a training epoch
 
-            # track metrics
+        self.model.eval()
 
-        # model.eval()
+        # store batch metrics with: self.train_metrics.store(acc, iou, loss)
+
         # run an evaluation epoch
 
-            # track metrics
+        self.model.train()
 
     def get_metrics(self, preds, labels):
-        # compute desired metrics for batch and return
-        return
+        """Compute batch accuracy and IoU
+        """
+        acc = self.metrics.get_accuracy_per_batch(preds, labels)
+        iou = self.metrics.get_iou_per_batch(preds, labels)
+        return acc, iou
 
-    def save(self):
-        # save current model instance
+    def save_model(self):
+        """Save checkpoint of current model
+        """
+        model_path = os.path.join(self.model_path, get_model_name(self.config, self.epoch))
+        torch.save(self.model.state_dict(), model_path)
+        optim_path = os.path.join(self.model_path, 'optimizer')
+        torch.save(self.optimizer.state_dict(), optim_path)
+
+    def load_model(self):
+        """Load a model and optimizer from checkpoint
+        """
+        # TODO
         return
 
 
@@ -67,7 +123,7 @@ if __name__ == '__main__':
     # parse CLI inputs
     parser = argparse.ArgumentParser(description='Training Configuration')
     parser.add_argument('--config', required=True, help='Path to configuration file')
-    parser.add_argument('--continue', type=bool, default=False, help="Continue training a previous model")
+    parser.add_argument('--pretrain', type=bool, default=False, help="Continue training a previous model")
     args = parser.parse_args()
 
     # create trainer and start training
