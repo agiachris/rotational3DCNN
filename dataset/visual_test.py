0b44fa54b9c6cb39691e402e6985055318344453
Polina Govorkova <catchingthegist@gmail.com>
2020-11-11 22:57:01 -0500

    separated visualization from main

---
+++ dataset/visual_test.py
@@ -0,0 +1,30 @@
+import os
+from utils import *
+from torch.utils.data import DataLoader
+from dataset.visualization import *
+import torch
+from dataset.data_utils import *
+from torch.utils.data import Dataset
+
+
+if __name__ == "__main__":
+	input_file = "/home/polinago/rotational3DCNN/data/1a04e3eab45ca15dd86060f189eb133__0__.sdf"
+	target_file = "/home/polinago/rotational3DCNN/data/1a04e3eab45ca15dd86060f189eb133__0__.df"
+
+	# get sdf input
+	input_tensor = tensor_from_file(input_file)
+	input_tensor = torch.from_numpy(input_tensor).unsqueeze(0)
+
+	# get df target
+	target_tensor = tensor_from_file(target_file)
+	target_tensor = torch.from_numpy(target_tensor).unsqueeze(0)
+
+	out = dict()
+	out['inputs'] = input_tensor
+	out['targets'] = target_tensor
+
+	# visualization of a random sample
+	sample = out
+	visualize_sdf(sample['inputs'].numpy())
+	visualize_df(sample['targets'].numpy())
+
